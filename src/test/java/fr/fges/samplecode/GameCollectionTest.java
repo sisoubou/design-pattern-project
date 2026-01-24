21c71d59418e12559701b5e8ec03b4b22c0d810e
Laure Massemin <laure.massemin@gmail.com>
2026-01-24 17:56:29 +0100

    test creation

---
+++ src/test/java/fr/fges/samplecode/GameCollectionTest.java
@@ -0,0 +1,45 @@
+package fr.fges.samplecode;
+
+import fr.fges.BoardGame;
+import fr.fges.GameCollection;
+import fr.fges.GameRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class GameCollectionTest {
+    @Test
+    void testAddGame(){
+        GameRepository mockRepo = Mockito.mock(GameRepository.class);
+        Mockito.when(mockRepo.load()).thenReturn(new ArrayList<>());
+        GameCollection collection = new GameCollection(mockRepo);
+
+        BoardGame myGame = new BoardGame("Uno", 2, 10, "Card");
+        collection.addGame(myGame);
+
+        assertEquals(1,collection.getGames().size(), "La collection devrait contenir 1 jeu");
+    }
+
+    @Test
+    void testRemoveGame() {
+        GameRepository mockRepo = Mockito.mock(GameRepository.class);
+
+        BoardGame existingGame = new BoardGame("Uno", 2, 10, "Card");
+        List<BoardGame> fakeList = new ArrayList<>();
+        fakeList.add(existingGame);
+
+        Mockito.when(mockRepo.load()).thenReturn(fakeList);
+        GameCollection collection = new GameCollection(mockRepo);
+
+        collection.removeGame(existingGame);
+
+        assertEquals(0,collection.getGames().size(), "La collection devrait etre vide");
+    }
+
+
+}
