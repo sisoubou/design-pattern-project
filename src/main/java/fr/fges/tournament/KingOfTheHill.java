8476b4660a8b6540da2057b9526b0868873be95d
sisoubou <serena.fadda8@gmail.com>
2026-04-06 22:00:07 +0200

    Add KingOfTheHill

---
+++ src/main/java/fr/fges/tournament/KingOfTheHill.java
@@ -0,0 +1,36 @@
+package fr.fges.tournament;
+
+import fr.fges.command.UserInput;
+import fr.fges.ui.TournamentUI;
+
+import java.util.List;
+
+public class KingOfTheHill implements Tournament {
+
+
+    @Override
+    public void playTournament(List<Player> players, UserInput userInput, TournamentUI ui) {
+        if (players.size() < 2) {
+            throw new IllegalArgumentException("Not enough players for a tournament.");
+        }
+
+        Player currentWinner = players.get(0);
+        int totalMatches = players.size() - 1;
+
+        for (int i = 1; i < players.size(); i++) {
+            Player challenger = players.get(i);
+
+            ui.onMatchStart(i, totalMatches, currentWinner.getName(), challenger.getName());
+
+            Match match = new Match(currentWinner, challenger, userInput);
+            Player matchWinner = match.playMatch();
+
+            if (currentWinner == matchWinner) {
+                ui.onKingRemains(currentWinner.getName());
+            } else {
+                currentWinner = challenger;
+                ui.onNewKing(currentWinner.getName());
+            }
+        }
+    }
+}
